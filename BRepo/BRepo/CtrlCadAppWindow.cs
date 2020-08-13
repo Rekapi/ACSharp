f39f5d0e2abe0094b9dcb12405b545b6868c420f
Mahmoud Rekapi <m.rekapi@gmail.com>
2020-08-13 12:37:44 +0200

    completing the last class

--- BRepo/BRepo/CtrlCadAppWindow.cs
+++ BRepo/BRepo/CtrlCadAppWindow.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+using System.Drawing;
+using System.Windows;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.Windows;
 
 namespace BRepo
 {
     class CtrlCadAppWindow
     {
+     // command for minimize and maximize the window
+     [CommandMethod("MinMaxApp")]
+     public void Min_max_Window()
+        {
+            // Minimize the application window
+            Application.MainWindow.WindowState = Window.State.Minimized;
+            System.Windows.Forms.MessageBox.Show("Minimized", "MinMax", System.Windows.Forms.MessageBoxButtons.OK,
+                                                                               System.Windows.Forms.MessageBoxIcon.None,
+                                                                               System.Windows.Forms.MessageBoxDefaultButton.Button1,
+                                                                               System.Windows.Forms.MessageBoxOptions.ServiceNotification);
+            // Maximize the application window 
+            Application.MainWindow.WindowState = Window.State.Maximized;
+            System.Windows.Forms.MessageBox.Show("Minimized", "MinMax");
+        }
     }
 }
